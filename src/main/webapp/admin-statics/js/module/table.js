9a5fc6dbb6bcb5bbd60bc64cb620c755c2236a3a
Ericadreamer <1979322310@qq.com>
2018-07-25 16:09:16 +0800

    列表修复全选按钮会刷新的问题，增加全选的js

---
+++ src/main/webapp/admin-statics/js/module/table.js
@@ -0,0 +1,37 @@
+function checkAll() {
+        var all=document.getElementById('all');//获取到点击全选的那个复选框的id
+        var one=document.getElementsByName('checkname[]');//获取到复选框的名称
+       
+        if(all.checked==true){//因为获得的是数组，所以要循环 为每一个checked赋值
+            for(var i=0;i<one.length;i++){
+                one[i].checked=true;
+            }
+ 
+        }else{
+            for(var j=0;j<one.length;j++){
+                one[j].checked=false;
+            }
+        }
+}
+$(function () {
+	 var all=document.getElementById('all');//获取到点击全选的那个复选框的id
+     var one=document.getElementsByName('checkname[]');//获取到复选框的名称
+    $("#allcheck").click(function(){
+        
+        if(all.checked==true){
+            all.checked=false;
+        for(var i=0;i<one.length;i++){
+             one[i].checked=false;
+             
+            }
+        }else{
+            all.checked=true;
+            for(var j=0;j<one.length;j++){
+              one[j].checked=true;
+               
+           }
+        }
+        
+    });
+	 
+});
