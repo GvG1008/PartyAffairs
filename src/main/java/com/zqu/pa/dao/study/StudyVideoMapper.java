5ec6d285ce878558b3faea9c8d99c4f26830a356
InnocentLEE <13822866200@163.com>
2018-08-10 14:17:29 +0800

    视频学习模块

--- src/main/java/com/zqu/pa/dao/study/StudyVideoMapper.java
+++ src/main/java/com/zqu/pa/dao/study/StudyVideoMapper.java
@@ -1,8 +1,10 @@
 package com.zqu.pa.dao.study;
 
 import java.util.List;
+import java.util.Map;
 
 import com.zqu.pa.entity.study.StudyVideo;
+import com.zqu.pa.entity.study.StudyVideoRecord;
 
 public interface StudyVideoMapper {
     int deleteByPrimaryKey(Integer videoId);
@@ -13,7 +15,13 @@ public interface StudyVideoMapper {
 
     StudyVideo selectByPrimaryKey(Integer videoId);
     
+    List<StudyVideo> selectPutonByLabelId(List<Integer> list);
+    
+    List<StudyVideo> selectMustPutonByUserId(String userId);
+    
     List<StudyVideo> selectPuton();
+    
+    float selectScheduleByVideoIdAndUserId(StudyVideoRecord record);
     
     int selectVideoIdByVideoPath(String videoPath);
 
