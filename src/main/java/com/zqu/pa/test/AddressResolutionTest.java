ed4c679a19ca49ac64db431c58c80d34ee0396d8
huanrong.chen@casstime.com <34814951+chr19960719@users.noreply.github.com>
2019-01-22 18:31:45 +0800

    修改pom

---
+++ src/main/java/com/zqu/pa/test/AddressResolutionTest.java
@@ -0,0 +1,14 @@
+package com.zqu.pa.test;
+
+import org.junit.Test;
+
+import com.zqu.pa.utils.CommonUtil;
+
+public class AddressResolutionTest {
+
+	@Test
+	public void test() {
+		System.out.println(CommonUtil.addressResolution("北京市市辖区朝阳区"));
+	}
+	
+}
--- src/main/java/com/zqu/pa/utils/CommonUtil.java
+++ src/main/java/com/zqu/pa/utils/CommonUtil.java
@@ -2,7 +2,16 @@ package com.zqu.pa.utils;
 
 import javax.servlet.http.HttpServletRequest;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CommonUtil {
 
@@ -237,5 +246,34 @@ public class CommonUtil {
             }
         }
         return false;
+    }
+    
+    /**
+     * 解析地址
+     * @author huanrong.chen
+     * @param address
+     * @return
+     */
+    public static List<Map<String,String>> addressResolution(String address){
+        String regex="(?<province>[^省]+自治区|.*?省|.*?行政区|.*?市)(?<city>[^市]+自治州|.*?地区|.*?行政单位|.+盟|市辖区|.*?市|.*?县)(?<county>[^县]+县|.+区|.+市|.+旗|.+海域|.+岛)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
+        Matcher m=Pattern.compile(regex).matcher(address);
+        String province=null,city=null,county=null,town=null,village=null;
+        List<Map<String,String>> table=new ArrayList<Map<String,String>>();
+        Map<String,String> row=null;
+        while(m.find()){
+            row=new LinkedHashMap<String,String>();
+            province=m.group("province");
+            row.put("province", province==null?"":province.trim());
+            city=m.group("city");
+            row.put("city", city==null?"":city.trim());
+            county=m.group("county");
+            row.put("county", county==null?"":county.trim());
+            town=m.group("town");
+            row.put("town", town==null?"":town.trim());
+            village=m.group("village");
+            row.put("village", village==null?"":village.trim());
+            table.add(row);
+        }
+        return table;
     }
 }
